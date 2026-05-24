015007aa65501071b5a0b8294c39e22aaace27cb
HasnainMurtaza265 <hasnainmurtaza265@gmail.com>
2026-05-24 13:47:30 +0500

    Fully Responsive

--- app.js
+++ app.js
@@ -5,53 +5,31 @@ var heroImages = [
 
 ];
 
-// Index track karne ke liye variable (0 matlab pehli image)
 var index = 0;
 
-// 2. Agli Image par jaane ka function
 function changeimg() {
-    // getElementById se HTML ka element uthaya
     var banner = document.getElementById("heroimg");
 
-    // Index ko 1 barhao
     index = index + 1;
 
-    // IF: Agar index barhte barhte array ki lambai (length) ke barabar ho jaye
     if (index == heroImages.length) {
-        index = 0; // Toh wapas pehli image (0) par le ao
+        index = 0;
     }
 
-    // Background image badal do
     banner.style.backgroundImage = "url('" + heroImages[index] + "')";
 }
 
-// 3. Pichli Image par jaane ka function
 function previmg() {
     var banner = document.getElementById("heroimg");
 
-    // Index ko 1 kam karo
     index = index - 1;
 
-    // IF: Agar index kam hote hote 0 se neeche (minus me) chala jaye
     if (index < 0) {
-        index = heroImages.length - 1; // Toh use aakhri image par le jao
+        index = heroImages.length - 1;
     }
 
-    // Background image badal do
     banner.style.backgroundImage = "url('" + heroImages[index] + "')";
 }
-
-
-
-
-
-
-
-
-
-
-
-
 
 
 
@@ -74,22 +52,6 @@ function slideCarousel(direction, buttonElement) {
 
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-// 🎁 READY TO USE HOVER BOX JAVASCRIPT 🎁
-
-// 1. Mouse lane par box dikhane ka function
 function showMenu() {
     var menu = document.getElementById("accountMenu");
 
@@ -98,7 +60,6 @@ function showMenu() {
     }
 }
 
-// 2. Mouse hatane par box chhupane ka function
 function hideMenu() {
     var menu = document.getElementById("accountMenu");
 
@@ -111,21 +72,6 @@ function hideMenu() {
 
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-// 🌍 SIRF LANGUAGE HOVER BOX KI JAVASCRIPT 🌍
-
-// 1. Language button par mouse aane par menu show karne ke liye
 function showLangMenu() {
     var langMenu = document.getElementById("langMenu");
 
@@ -134,7 +80,6 @@ function showLangMenu() {
     }
 }
 
-// 2. Language button se mouse hatne par menu hide karne ke liye
 function hideLangMenu() {
     var langMenu = document.getElementById("langMenu");
 
